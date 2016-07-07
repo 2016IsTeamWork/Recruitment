1ec991c39118f6e2dd92a5aa6ed917322145e182
TakeActionNow <bartkrav@gmail.com>
2016-07-07 09:01:30 +0200

    You can now select the VacancyTabel hyperlink and it will SPA load the file and also load the information from the back-end.

--- RecruitementWebsite/Scripts/app/Main/RecruitmentAppRouting.js
+++ RecruitementWebsite/Scripts/app/Main/RecruitmentAppRouting.js
@@ -23,6 +23,11 @@ MyApp.config(function ($routeProvider) {
             controller: 'contactController'
         })
 
+        .when('/VacancyTabel', {
+            templateUrl: 'Scripts/app/HTMLPages/VacancyTabel.html',
+            controller: 'safeCtrl'
+        })
+
         .otherwise({
             redirectTo: '/'
         });
@@ -41,3 +46,35 @@ MyApp.controller('aboutController', function ($scope) {
 MyApp.controller('contactController', function ($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
 });
+
+MyApp.controller('safeCtrl', ['$scope', '$http', '$timeout', '$interval', function ($scope, $http, $timeout, $interval) {
+
+    var DataToAlert;
+
+    pollData();
+
+    function pollData() {
+        
+        var collection = [];
+
+        $interval(function () {
+
+            $http.get("/Home/GetVacancyList").then(function (result) {
+
+                angular.forEach(result.data, function (item, key) {
+                    console.log("DataKey: " + key + " DataValue: " + item);
+                    console.log("DataKey: " + key + "Company: " + item.Company);
+
+                    var row = { Title: item.Title, Company: item.Company, Salary: item.Salary }
+
+                    console.log("Row: " + row + " Row -> Title: " + row.Title +  " Row -> Company: " + row.Company + " Row -> Salary: " + row.Salary);
+                    collection.push(row);
+                });
+
+                $scope.rowCollection = collection;
+            });
+
+        }, 20000);
+    }
+}]);
+ 
